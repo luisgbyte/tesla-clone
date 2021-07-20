1897bba0fb74acbbe27e2aedea5da4f907e80342
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2021-07-20 15:43:25 -0300

    revealing and styling overlays

--- src/components/Model/ModelsWrapper/index.tsx
+++ src/components/Model/ModelsWrapper/index.tsx
@@ -2,7 +2,7 @@ import React, { useCallback, useRef, useState } from "react";
 
 import ModelsContext, { CarModel } from "../ModelsContext";
 
-import { Container } from "./styles";
+import { Container, OverlaysRoot, ModelOverlay } from "./styles";
 
 const ModelsWrapper: React.FC = ({ children }) => {
   const wrapperRef = useRef<HTMLDivElement>(null);
@@ -33,7 +33,14 @@ const ModelsWrapper: React.FC = ({ children }) => {
         unregisterModel,
         getModelByName
     }}>
-      <Container ref={wrapperRef}>{children}</Container>
+      <Container ref={wrapperRef}>
+        <OverlaysRoot>
+            {registeredModels.map(item => (
+              <ModelOverlay key={item.modelName}>{item.overlayNode}</ModelOverlay>
+            ))}
+        </OverlaysRoot>
+        {children}
+        </Container>
     </ModelsContext.Provider>
   );
 };
